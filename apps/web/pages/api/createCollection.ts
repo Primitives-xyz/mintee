d65d23d2d578acf3ab1a5d615f45a77f6afcd13f
Nicholas Oxford <maheledaily@gmail.com>
2023-03-08 16:08:19 -0700

    collection endpoint

---
+++ apps/web/pages/api/createCollection.ts
@@ -0,0 +1,46 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { Keypair } from "@solana/web3.js";
+import base58 from "bs58";
+import { WrappedConnection } from "../../utils/wrappedConnection";
+import { initCollection } from "../../utils/mint";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const apiKey = process.env["API_KEY"];
+  if (!apiKey) {
+    throw new Error("Api key must be provided via API_KEY env var");
+  }
+  const connectionString = `https://rpc-devnet.helius.xyz?api-key=${apiKey}`;
+  const secretKey = process.env["SECRET_KEY"];
+  if (!secretKey) {
+    throw new Error(
+      "Wallet secret key must be provided via SECRET_KEY env var"
+    );
+  }
+
+  let decodedSecretKey;
+  try {
+    decodedSecretKey = base58.decode(secretKey);
+  } catch {
+    throw new Error(
+      "Invalid secret key provided. Must be a base 58 encoded string."
+    );
+  }
+  const ownerWallet = Keypair.fromSecretKey(decodedSecretKey);
+  const connectionWrapper = new WrappedConnection(
+    ownerWallet,
+    connectionString
+  );
+  const {
+    collectionMint,
+    collectionMetadataAccount,
+    collectionMasterEditionAccount,
+  } = await initCollection(connectionWrapper, ownerWallet);
+  return res.json({
+    collectionMint,
+    collectionMetadataAccount,
+    collectionMasterEditionAccount,
+  });
+}
---
+++ apps/web/pages/api/createTree.ts
@@ -0,0 +1,116 @@
+import { Keypair } from "@solana/web3.js";
+import base58 from "bs58";
+import { NextApiRequest, NextApiResponse } from "next";
+import { initTree } from "../../utils/mint";
+import { WrappedConnection } from "../../utils/wrappedConnection";
+
+export default async function hello(req: NextApiRequest, res: NextApiResponse) {
+  const apiKey = process.env["API_KEY"];
+  if (!apiKey) {
+    throw new Error("Api key must be provided via API_KEY env var");
+  }
+
+  const secretKey = process.env["SECRET_KEY"];
+  if (!secretKey) {
+    throw new Error(
+      "Wallet secret key must be provided via SECRET_KEY env var"
+    );
+  }
+  let decodedSecretKey;
+  try {
+    decodedSecretKey = base58.decode(secretKey);
+  } catch {
+    throw new Error(
+      "Invalid secret key provided. Must be a base 58 encoded string."
+    );
+  }
+
+  const ownerWallet = Keypair.fromSecretKey(decodedSecretKey);
+  console.log("Owner wallet: " + ownerWallet.publicKey);
+
+  const connectionString = `https://rpc-devnet.helius.xyz?api-key=${apiKey}`;
+  const connectionWrapper = new WrappedConnection(
+    ownerWallet,
+    connectionString
+  );
+
+  // Fixed wallet to manage the merkle tree used to store the collection.
+  const treeWallet = Keypair.generate();
+  console.log("Tree wallet: " + treeWallet.publicKey);
+  console.log("Creating merkle tree.");
+  const treeAddress = await initTree(
+    connectionWrapper,
+    ownerWallet,
+    treeWallet
+  );
+  console.log("Tree address: " + treeAddress);
+  const {
+    collectionMint,
+    collectionMetadataAccount,
+    collectionMasterEditionAccount,
+  } = await initCollection(connectionWrapper, ownerWallet);
+  console.log("\n===Collection Details===");
+  console.log("Mint account: " + collectionMint.toBase58());
+  console.log("Metadata account: " + collectionMetadataAccount.toBase58());
+  console.log(
+    "Master edition account: " + collectionMasterEditionAccount.toBase58()
+  );
+  console.log("\n");
+
+  // Mint a compressed NFT
+  const nftArgs = {
+    name: "Compression Test",
+    symbol: "COMP",
+    uri: "https://arweave.net/gfO_TkYttQls70pTmhrdMDz9pfMUXX8hZkaoIivQjGs",
+    creators: [],
+    editionNonce: 253,
+    tokenProgramVersion: TokenProgramVersion.Original,
+    tokenStandard: TokenStandard.NonFungible,
+    uses: null,
+    collection: null,
+    primarySaleHappened: false,
+    sellerFeeBasisPoints: 0,
+    isMutable: false,
+  };
+  const sig = await mintCompressedNft(
+    connectionWrapper,
+    nftArgs,
+    ownerWallet,
+    treeWallet,
+    collectionMint,
+    collectionMetadataAccount,
+    collectionMasterEditionAccount
+  );
+  console.log("Minted compressed nft with txn: " + sig);
+
+  // Get the NFT mint ID from the merkle tree.
+  const treeAccount = await ConcurrentMerkleTreeAccount.fromAccountAddress(
+    connectionWrapper,
+    treeWallet.publicKey
+  );
+  // Get the most rightmost leaf index, which will be the most recently minted compressed NFT.
+  // Alternatively you can keep a counter that is incremented on each mint.
+  const leafIndex = treeAccount.tree.rightMostPath.index - 1;
+  const assetId = await getCompressedNftId(treeWallet, leafIndex);
+  console.log("Minted asset: " + assetId);
+
+  // Fixed wallet to receive the NFT when we test transfer.
+  const newOwnerWallet = Keypair.fromSeed(
+    new TextEncoder().encode("next wallet".padEnd(32, "\0"))
+  );
+  console.log("New owner wallet: " + newOwnerWallet.publicKey.toBase58());
+
+  console.log("\n===Transfer===");
+  console.log("Transfer to new wallet.");
+  await transferAsset(
+    connectionWrapper,
+    ownerWallet,
+    newOwnerWallet,
+    assetId.toBase58()
+  );
+  console.log(
+    "Successfully transferred nft to wallet: " +
+      newOwnerWallet.publicKey.toBase58()
+  );
+  return res.json("Hello world! Tree created:" + treeAddress.toString());
+}
