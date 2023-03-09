5c15f270094b9654a1e27a01971cf9a97ef064a2
Nicholas Oxford <maheledaily@gmail.com>
2023-03-08 22:07:05 -0700

    asset api

---
+++ apps/web/utils/connectionWrapper.ts
@@ -0,0 +1,27 @@
+import base58 from "bs58";
+import { WrappedConnection } from "./wrappedConnection";
+import { Keypair } from "@solana/web3.js";
+export function getConnectionWrapper() {
+  const apiKey = process.env["API_KEY"];
+  if (!apiKey) {
+    throw new Error("Api key must be provided via API_KEY env var");
+  }
+  const connectionString = `https://rpc-devnet.helius.xyz?api-key=${apiKey}`;
+  const secretKey = process.env["SECRET_KEY"];
+  if (!secretKey) {
+    throw new Error(
+      "Wallet secret key must be provided via SECRET_KEY env var"
+    );
+  }
+
+  let decodedSecretKey;
+  try {
+    decodedSecretKey = base58.decode(secretKey);
+  } catch {
+    throw new Error(
+      "Invalid secret key provided. Must be a base 58 encoded string."
+    );
+  }
+  const ownerWallet = Keypair.fromSecretKey(decodedSecretKey);
+  return new WrappedConnection(ownerWallet, connectionString);
+}
