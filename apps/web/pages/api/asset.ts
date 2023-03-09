73c3ada8642103ea8c89c83186cc745f1fc8a0f4
Nicholas Oxford <maheledaily@gmail.com>
2023-03-08 23:54:25 -0700

    idk tings

--- apps/web/pages/api/asset.ts
+++ apps/web/pages/api/asset.ts
@@ -9,6 +9,12 @@ export default async function handler(
   // grab the assetId from the request
   const { assetId } = req.query;
 
-  const result = await connectionWrapper.getAsset(assetId);
+  const result = await connectionWrapper.getAsset(assetId).catch((e) => {
+    console.error(e);
+    return res.status(500).json({ error: e.message });
+  });
+  if (!result) {
+    return res.status(500).json({ error: "Failed to get asset" });
+  }
   return res.json(result);
 }
