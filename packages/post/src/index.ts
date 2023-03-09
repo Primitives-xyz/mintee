7363930190f295e96bf743f603999cddb7428dd4
Nicholas Oxford <maheledaily@gmail.com>
2023-03-09 16:52:22 -0700

    another small fix

--- packages/post/src/index.ts
+++ packages/post/src/index.ts
@@ -1,12 +1,14 @@
-export default function post(url: string, data?: any, authorization?: string) {
-  return fetch(url, {
+export default async function post(
+  url: string,
+  data?: any,
+  authorization?: string
+) {
+  return await fetch(url, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       Authorization: authorization || "",
     },
     body: data ? JSON.stringify(data) : undefined,
-  }).catch((e) => {
-    throw new Error(e);
   });
 }
