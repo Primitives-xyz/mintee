c362bddc2561c844581a7c32730eff3d9d66bc1e
Nicholas Oxford <maheledaily@gmail.com>
2023-03-14 11:51:24 -0600

    add readme

--- apps/workers/src/routes/index.ts
+++ apps/workers/src/routes/index.ts
@@ -3,7 +3,9 @@ import { uploadMetadataRoute } from "./uploadMetadata";
 import { mintTreeRoutes } from "./mintTree";
 import { nftInfoRoute } from "./nftInfo";
 import { nftStatusRoute } from "./nftStatus";
+import { helloRoute } from "./hello";
 export {
+  helloRoute,
   mintRoute,
   uploadMetadataRoute,
   mintTreeRoutes,
