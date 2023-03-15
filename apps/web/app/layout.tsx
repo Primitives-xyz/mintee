5a3c5f70fc56ed40d4b362ce9595b6ce23be2f8d
Nicholas Oxford <maheledaily@gmail.com>
2023-03-14 23:04:24 -0600

    route test

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,5 +1,9 @@
-import { RedirectToSignIn } from "@clerk/nextjs";
-import { ClerkProvider, SignedIn, SignedOut } from "@clerk/nextjs/app-beta";
+import {
+  auth,
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+} from "@clerk/nextjs/app-beta";
 import Header from "../components/Header";
 import "../styles/globals.css";
 export const metadata = {
@@ -14,13 +18,14 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const { userId } = auth();
   return (
     <ClerkProvider>
       <html lang="en">
         <body>
           <SignedIn>
             <div className="bg-[#111C27] text-white max-w-full min-h-screen mx-auto flex-col items-center justify-center py-2">
-              <Header />
+              <Header userId={userId ?? undefined} />
 
               {children}
             </div>
