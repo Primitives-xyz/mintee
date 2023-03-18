4b88cac07ff35207477097ffe173bc18abb604cb
Nicholas Oxford <maheledaily@gmail.com>
2023-03-18 14:43:49 -0600

    Revert "remove edge padge"
    This reverts commit 1ed3c22c237615ecb0ea1aac0c95d549a704bf7a.

--- apps/web/app/(admin)/dashboard/loading.tsx
+++ apps/web/app/(admin)/dashboard/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
   return (
-    <button type="button" className="" disabled>
+    <button type="button" className="bg-indigo-500 ..." disabled>
       <svg className="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24"></svg>
       Processing...
     </button>
