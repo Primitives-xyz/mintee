ffd5bcb7b51b0ae8956142631bd36ec3236fa98c
Nicholas Oxford <maheledaily@gmail.com>
2023-03-19 22:08:38 -0600

    small api

---
+++ apps/web/app/(admin)/dashboard/apiKeys/loading.tsx
@@ -0,0 +1,10 @@
+export default async function Loading() {
+  return (
+    <>
+      <h2 className="text-2xl min-w-full shadow-md bg-[#111C27]/75 p-2 rounded-lg">
+        API Tokens
+      </h2>
+      <div>Grabbing API keys..</div>
+    </>
+  );
+}
