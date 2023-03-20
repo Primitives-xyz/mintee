432233a663bdb76bb5759811c56658bf560e4577
Nicholas Oxford <maheledaily@gmail.com>
2023-03-19 21:52:05 -0600

    loading for onboarding

---
+++ apps/web/app/(home)/onboard/loading.tsx
@@ -0,0 +1,8 @@
+export function loadingAPI() {
+  return (
+    <div className="flex justify-center items-center flex-row">
+      <h1>Loading...</h1>
+      <h1>Gotta grab your API keys</h1>
+    </div>
+  );
+}
