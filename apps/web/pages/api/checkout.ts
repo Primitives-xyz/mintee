c275ef95ccccb44a0645f0ce70f10c0004f797bf
Nicholas Oxford <maheledaily@gmail.com>
2023-03-20 17:37:32 -0600

    i think we got it finally

--- apps/web/pages/api/checkout.ts
+++ apps/web/pages/api/checkout.ts
@@ -1,13 +1,29 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import Stripe from "stripe";
-
+import multiparty from "multiparty";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2022-11-15",
 });
+
+//set bodyparser
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const form = new multiparty.Form();
+  const data = await new Promise((resolve, reject) => {
+    form.parse(req, function (err: any, fields: any, files: any) {
+      if (err) reject({ err });
+      resolve({ fields, files });
+    });
+  });
+  console.log(`data: `, JSON.stringify(data));
   if (req.method === "POST") {
     try {
       console.log("req.body", req);
