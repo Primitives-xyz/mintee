c0864c9038752a90b7942b13e3a08bcda2bb31b9
Nicholas Oxford <maheledaily@gmail.com>
2023-03-20 21:11:51 -0600

    lets launch

--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -7,8 +7,18 @@ const config: DocsThemeConfig = {
   project: {
     link: "https://github.com/shuding/nextra-docs-template",
   },
-
-  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+  head: (
+    <>
+      <title>Mintee Docs!</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Mintee Docs!" />
+      <meta
+        property="og:description"
+        content="The easiest way to mint NFTs at scale"
+      />
+    </>
+  ),
+  docsRepositoryBase: "https://docs.mintee.io",
   footer: {
     text: "Mintee API",
   },
