e225714a3750d6ea484b6185ccdd61d693030ecc
Nicholas Oxford <maheledaily@gmail.com>
2023-03-20 20:28:30 -0600

    sooo close!

--- apps/web/app/(home)/Header.tsx
+++ apps/web/app/(home)/Header.tsx
@@ -1,7 +1,16 @@
 // import { UserButton } from "@clerk/nextjs";
 import { UserButton } from "@clerk/nextjs/app-beta";
 import Link from "next/link";
-
+import HeaderCheckout from "./onboard/headerCheckout";
+import StripeCheckout from "./onboard/stripeCheckout";
+async function callCheckout(userId: string) {
+  await fetch("/api/checkout", {
+    method: "POST",
+    body: JSON.stringify({
+      userId,
+    }),
+  });
+}
 export default function Header({ userId }: { userId?: string }) {
   return (
     <header className="flex flex-col xs:flex-row  justify-between items-center w-full mt-3 border-b pb-7 sm:px-4 px-2 border-gray-500 gap-2">
@@ -36,13 +45,7 @@ export default function Header({ userId }: { userId?: string }) {
             >
               <p>Dashboard</p>
             </Link>
-            <a
-              className="flex max-w-fit items-center justify-center  rounded-full border border-blue-600 text-white px-5 py-2 text-sm shadow-md hover:bg-blue-500 bg-blue-600 font-medium transition"
-              href="/docs"
-            >
-              <p>Upgrade</p>
-              <RightArrow />
-            </a>
+            <HeaderCheckout />
           </>
         )}
         <div className="flex justify-center items-center">
