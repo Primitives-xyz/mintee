a17cb4ac71d5d08d26567f2e2d0b5a0d4af0daa4
Nicholas Oxford <maheledaily@gmail.com>
2023-03-26 22:23:49 -0600

    yoooo 2

--- apps/token/start.sh
+++ apps/token/start.sh
@@ -2,4 +2,5 @@
 
 TIME_TO_SHUTDOWN="${IDLE_TIME_TO_SHUTDOWN:-120}"
 node server &
-/tired-proxy --port 8080 --host http://localhost:3000 --time $TIME_TO_SHUTDOWN
+/tired-proxy --port 8080 --host http://localhost:8080 --time $TIME_TO_SHUTDOWN
+
